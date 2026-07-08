6c2100fdc3af963ebcba3483b8dc493dc49e1929
Giorgi <tserediani.g@gmail.com>
2026-07-08 17:51:36 +0400

    refactor: update calculate_sum function to web application and included necessary dependencies

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
-def calculate_sum(x: int, y: int) -> int:
+from fastapi import FastAPI, Path
+
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/")
+def calculate_sum(x: Annotated[int, Path], y: Annotated[int, Path]) -> int:
     return x + y
-
-
-def main() -> None:
-    custom_sum = calculate_sum(1, 2)
-    print(custom_sum)
-
-
-if __name__ == "__main__":
-    main()
