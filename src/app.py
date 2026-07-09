c3f5d61242842ce42373cc1cb5ed3f8dce0c3f26
Giorgi Tserediani <tserediani.g@gmail.com>
2026-07-09 15:19:44 +0400

    feat: add proper root route and separate calculate_sum route

--- src/app.py
+++ src/app.py
@@ -5,6 +5,11 @@ from typing import Annotated
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/calculate_sum")
 def calculate_sum(x: Annotated[int, Path], y: Annotated[int, Path]) -> int:
     return x + y
+
+
+@app.get("/")
+def root() -> dict[str, str]:
+    return {"status": "ok"}
